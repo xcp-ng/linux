d047a63adb612ccad6a427ce41cab5b9bdde67e8
David Vrabel <david.vrabel@citrix.com>
2014-07-10 13:10:04 +0100

    pci: export pci_probe_reset_function()
    The xen-pciback needs to test if a per-function reset is available so
    it can provide an interface for a safe bus reset instead.
    
    Signed-off-by: David Vrabel <david.vrabel@citrix.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -33,7 +33,6 @@ enum pci_mmap_api {
 int pci_mmap_fits(struct pci_dev *pdev, int resno, struct vm_area_struct *vmai,
 		  enum pci_mmap_api mmap_api);
 
-int pci_probe_reset_function(struct pci_dev *dev);
 int pci_bridge_secondary_bus_reset(struct pci_dev *dev);
 int pci_bus_error_reset(struct pci_dev *dev);
 int __pci_reset_bus(struct pci_bus *bus);
