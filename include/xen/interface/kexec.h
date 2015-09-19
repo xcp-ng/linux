4189aa82f8e0406e424f9e02c9ee1fdaf18fc71a
Vates Git Kernel Importer <gitimporter@vates.tech>
2015-09-19 23:58:42 +0200

    Reserve the crashkernel region in the E820. This prevents oustanding DMA operations overwriting the crashkernel region when the IOMMU is disabled during kexec. Normally, the IOMMU would not be used for dom0, but it is used for PV-IOMMU.
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- include/xen/interface/kexec.h
+++ include/xen/interface/kexec.h
@@ -2,9 +2,19 @@
 #define _XEN_KEXEC_H
 
 #define KEXEC_CMD_kexec                    0
+#define KEXEC_CMD_kexec_get_range          3
 typedef struct xen_kexec_exec {
 	int type;
 } xen_kexec_exec_t;
+
+#define KEXEC_RANGE_MA_CRASH      0 /* machine address and size of crash area */
+
+typedef struct xen_kexec_range {
+	int range;
+	int nr;
+	unsigned long size;
+	unsigned long start;
+} xen_kexec_range_t;
 
 #endif
 
