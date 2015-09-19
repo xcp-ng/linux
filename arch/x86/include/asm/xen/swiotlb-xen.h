742fe39218e0f8b06522416369f7f52de27da474
Corentin Oparowski <corentin.oparowski@vates.tech>
2015-09-19 08:09:42 +0200

    The objective is to ensure we have a large enough collection of 2M "slabs", each of which is physically contiguous, in 32-bit address space, to service DMA for devices that are limited to a 32-bit address space. Ideally we want a total of 128M (a separate patch updates this from the kernel default of 64M), but we are content with less, and will retry three times, halving the allocation each time, before giving up.
    A unique Xen problem results from memory being exchanged with the
    hypervisor, whereby the kernel offers an amount of memory to the
    hypervisor, and the hypervisor returns different memory, in this case
    contiguous, low-address memory.  This enables VMs (including dom0) to
    remain of the exact same size, which has significant benefits in terms
    of VM and memory management.
    
    If, however, not enough of the desired memory is available, the hypervisor
    will return what it's got, and we have a partial success.  In this case
    retrying is guaranteed to fail, since the hypervisor has already exchanged
    everything it can, and we would end up giving up.  To avoid this we
    record how much memory we actually got by way of an extra paramemeter
    ("contig_pages"), and if this is enough (at time of writing we whink
    down to 1/8th of the default request is OK) we take it and run.

--- arch/x86/include/asm/xen/swiotlb-xen.h
+++ arch/x86/include/asm/xen/swiotlb-xen.h
@@ -2,7 +2,8 @@
 #ifndef _ASM_X86_SWIOTLB_XEN_H
 #define _ASM_X86_SWIOTLB_XEN_H
 
-int xen_swiotlb_fixup(void *buf, unsigned long nslabs);
+int xen_swiotlb_fixup(void *buf, unsigned long nslabs,
+		      unsigned long *contig_pages);
 int xen_create_contiguous_region(phys_addr_t pstart, unsigned int order,
 				unsigned int address_bits,
 				dma_addr_t *dma_handle);
