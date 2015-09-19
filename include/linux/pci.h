36c9f2985bd52ef3106031ebe9258ac10f38275a
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    Some devices advertise FLReset+ but do not support it. Add a quirk for some devices to fallback to an alternate reset method.
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -182,6 +182,8 @@ enum pci_dev_flags {
 	PCI_DEV_FLAGS_NO_PM_RESET = (__force pci_dev_flags_t) (1 << 7),
 	/* Get VPD from function 0 VPD */
 	PCI_DEV_FLAGS_VPD_REF_F0 = (__force pci_dev_flags_t) (1 << 8),
+	/* Do not use FLR reset even if device advertises FLReset+ */
+	PCI_DEV_FLAGS_NO_FLR_RESET = (__force pci_dev_flags_t) (1 << 9),
 };
 
 enum pci_irq_reroute_variant {
