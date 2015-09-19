be196b1199d6f44125c7807cb19c57a5f0e328ed
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 07:09:42 +0100

    CA-135938-nfs-disconnect-on-rpc-retry

--- net/sunrpc/xprtsock.c
+++ net/sunrpc/xprtsock.c
@@ -2280,6 +2280,8 @@ static void xs_tcp_shutdown(struct rpc_xprt *xprt)
 		break;
 	case TCP_CLOSE:
 	case TCP_TIME_WAIT:
+	case TCP_FIN_WAIT1:
+	case TCP_FIN_WAIT2:
 		xs_reset_transport(transport);
 	}
 }
