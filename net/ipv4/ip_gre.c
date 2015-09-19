845b6c30ea1631cbc40d16ec3149440d236a53ba
Vates Git Kernel Importer <gitimporter@vates.tech>
2015-09-19 23:58:42 +0200

    enable-fragmention-gre-packets.patch

--- net/ipv4/ip_gre.c
+++ net/ipv4/ip_gre.c
@@ -572,6 +572,7 @@ static void gre_fb_xmit(struct sk_buff *skb, struct net_device *dev,
 			 (flags & TUNNEL_SEQ) ? htonl(atomic_fetch_inc(&tunnel->o_seqno)) : 0);
 
 	df = key->tun_flags & TUNNEL_DONT_FRAGMENT ?  htons(IP_DF) : 0;
+	skb->ignore_df = 1;
 
 	iptunnel_xmit(skb->sk, rt, skb, fl.saddr, key->u.ipv4.dst, IPPROTO_GRE,
 		      key->tos, key->ttl, df, false);
