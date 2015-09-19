2339ea572021db6bc0d1ee87d554823845298f3f
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    If the host has more CPUs than the kernel is compiled for, warnings appear in the kernel log. This doesn't make sense for dom0 because the number of CPUs is set by Xen but it processes the host ACPI tables which lists all the CPUs. Hide the warning since it is expected.
    Example warning:
    [ 0.000000] ACPI: NR_CPUS/possible_cpus limit of 24 reached. Processor 27/0x3c ignored.

--- arch/x86/kernel/apic/apic.c
+++ arch/x86/kernel/apic/apic.c
@@ -2341,9 +2341,10 @@ int generic_processor_info(int apicid, int version)
 	if (num_processors >= nr_cpu_ids) {
 		int thiscpu = max + disabled_cpus;
 
-		pr_warning("APIC: NR_CPUS/possible_cpus limit of %i "
-			   "reached. Processor %d/0x%x ignored.\n",
-			   max, thiscpu, apicid);
+		if (!xen_initial_domain())
+			pr_warning("APIC: NR_CPUS/possible_cpus limit of %i "
+				   "reached. Processor %d/0x%x ignored.\n",
+				   max, thiscpu, apicid);
 
 		disabled_cpus++;
 		return -EINVAL;
