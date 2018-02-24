oracle/linux-uek

x86,xen: correct dma_get_required_mask() for Xen PV guests
On systems where DMA addresses and physical addresses are not 1:1
(such as Xen PV guests), the generic dma_get_required_mask() will not
return the correct mask (since it uses max_pfn).

Some device drivers (such as mptsas, mpt2sas) use
dma_get_required_mask() to set device DMA masks to allow them to use
only 32-bit DMA addresses in hardware structures.  This results in
unnecessary use of the SWIOTLB if DMA addresses are more than 32-bits,
impacting performance significantly.

Provide an arch-specific dma_get_required_mask() that defaults to the
generic dma_get_required_mask_from_pfn().

Under Xen, the required DMA mask can then be set to always 64-bits.

Signed-off-by: David Vrabel <david.vrabel@citrix.com>
Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

Orabug: 27145524

checkpatch.pl reports one error:
  ERROR: #define of 'ARCH_HAS_DMA_GET_REQUIRED_MASK' is wrong - use
         Kconfig variables or standard guards instead
  #44: FILE: arch/x86/include/asm/device.h:24:
  +#define ARCH_HAS_DMA_GET_REQUIRED_MASK

Error is being ignored due to existing code with similar #defines that
were added before the ARCH_HAS check was added to checkpatch.pl.
Refer to the following source for similar #defines:
     arch/powerpc/include/asm/device.h:57
     arch/tile/include/asm/dma-mapping.h:24
     arch/ia64/include/asm/dma-mapping.h:14

(cherry picked from commit a7b11ecc28b75416d8580c6d7c6afecf61d866dc)
Signed-off-by: Matt Keenan <matt.keenan@oracle.com>
Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

arch/x86/kernel/pci-dma.c
@@ -149,6 +149,14 @@ bool arch_dma_alloc_attrs(struct device **dev, gfp_t *gfp)
 }
 EXPORT_SYMBOL(arch_dma_alloc_attrs);
 
+u64 dma_get_required_mask(struct device *dev)
+{
+	if (dma_ops->get_required_mask)
+		return dma_ops->get_required_mask(dev);
+	return dma_get_required_mask_from_max_pfn(dev);
+}
+EXPORT_SYMBOL(dma_get_required_mask);
+
 /*
  * See <Documentation/x86/x86_64/boot-options.txt> for the iommu kernel
  * parameter documentation.

xcp-ng/linux

arch/x86/kernel/pci-dma.c
@@ -89,6 +89,14 @@ bool arch_dma_alloc_attrs(struct device **dev)
 }
 EXPORT_SYMBOL(arch_dma_alloc_attrs);
 
+u64 dma_get_required_mask(struct device *dev)
+{
+	if (dma_ops->get_required_mask)
+		return dma_ops->get_required_mask(dev);
+	return dma_get_required_mask_from_max_pfn(dev);
+}
+EXPORT_SYMBOL_GPL(dma_get_required_mask);
+
 /*
  * See <Documentation/x86/x86_64/boot-options.txt> for the iommu kernel
  * parameter documentation.