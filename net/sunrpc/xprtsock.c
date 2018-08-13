794159b068215ebb5ed81efd776244908f79b344
Trond Myklebust <trond.myklebust@hammerspace.com>
2018-08-13 16:50:49 -0400

    SUNRPC: Move reset of TCP state variables into the reconnect code
    Rather than resetting state variables in socket state_change() callback,
    do it in the sunrpc TCP connect function itself.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- net/sunrpc/xprtsock.c
+++ net/sunrpc/xprtsock.c
@@ -1613,13 +1613,6 @@ static void xs_tcp_state_change(struct sock *sk)
 	case TCP_ESTABLISHED:
 		spin_lock(&xprt->transport_lock);
 		if (!xprt_test_and_set_connected(xprt)) {
-
-			/* Reset TCP record info */
-			transport->tcp_offset = 0;
-			transport->tcp_reclen = 0;
-			transport->tcp_copied = 0;
-			transport->tcp_flags =
-				TCP_RCV_COPY_FRAGHDR | TCP_RCV_COPY_XID;
 			xprt->connect_cookie++;
 			clear_bit(XPRT_SOCK_CONNECTING, &transport->sock_state);
 			xprt_clear_connecting(xprt);
@@ -2401,6 +2394,12 @@ static int xs_tcp_finish_connecting(struct rpc_xprt *xprt, struct socket *sock)
 		goto out;
 
 	xs_set_memalloc(xprt);
+
+	/* Reset TCP record info */
+	transport->tcp_offset = 0;
+	transport->tcp_reclen = 0;
+	transport->tcp_copied = 0;
+	transport->tcp_flags = TCP_RCV_COPY_FRAGHDR | TCP_RCV_COPY_XID;
 
 	/* Tell the socket layer to start connecting... */
 	xprt->stat.connect_count++;
