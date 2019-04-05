6fd84055078ea1ce5c3792a1919ec5879c3c622b
Andreas Gruenbacher <agruenba@redhat.com>
2019-04-05 12:16:14 +0100

    gfs2: Rename sd_log_le_{revoke,ordered}
    Rename sd_log_le_revoke to sd_log_revokes and sd_log_le_ordered to
    sd_log_ordered: not sure what le stands for here, but it doesn't add
    clarity, and if it stands for list entry, it's actually confusing as
    those are both list heads but not list entries.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    
    [quentin: conflict due to removal of c9d1cef44d40 ("gfs2: Replace
    gl_revokes with a GLF flag") which was added/reverted in the
    patch-queue (easier to simply not apply both)]

--- fs/gfs2/lops.c
+++ fs/gfs2/lops.c
@@ -316,7 +316,7 @@ static struct bio *gfs2_log_get_bio(struct gfs2_sbd *sdp, u64 blkno,
  * @sdp: the filesystem
  * @page: the page to write
  * @size: the size of the data to write
- * @offset: the offset within the page 
+ * @offset: the offset within the page
  * @blkno: block number of the log entry
  *
  * Try and add the page segment to the current bio. If that fails,
@@ -345,7 +345,7 @@ void gfs2_log_write(struct gfs2_sbd *sdp, struct page *page,
  * gfs2_log_write_bh - write a buffer's content to the log
  * @sdp: The super block
  * @bh: The buffer pointing to the in-place location
- * 
+ *
  * This writes the content of the buffer to the next available location
  * in the log. The buffer will be unlocked once the i/o to the log has
  * completed.
@@ -621,7 +621,7 @@ static void revoke_lo_before_commit(struct gfs2_sbd *sdp, struct gfs2_trans *tr)
 {
 	struct gfs2_meta_header *mh;
 	unsigned int offset;
-	struct list_head *head = &sdp->sd_log_le_revoke;
+	struct list_head *head = &sdp->sd_log_revokes;
 	struct gfs2_bufdata *bd;
 	struct page *page;
 	unsigned int length;
@@ -659,7 +659,7 @@ static void revoke_lo_before_commit(struct gfs2_sbd *sdp, struct gfs2_trans *tr)
 
 static void revoke_lo_after_commit(struct gfs2_sbd *sdp, struct gfs2_trans *tr)
 {
-	struct list_head *head = &sdp->sd_log_le_revoke;
+	struct list_head *head = &sdp->sd_log_revokes;
 	struct gfs2_bufdata *bd;
 	struct gfs2_glock *gl;
 
@@ -882,4 +882,3 @@ const struct gfs2_log_operations *gfs2_log_ops[] = {
 	&gfs2_revoke_lops,
 	NULL,
 };
-
