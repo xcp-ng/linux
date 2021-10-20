7c308dad3eeec2aad7f98c456d3e195c5d42fc13
Ye Bin <yebin10@huawei.com>
2021-10-20 15:39:59 +0800

    nbd: Fix use-after-free in pid_show
    I got issue as follows:
    [  263.886511] BUG: KASAN: use-after-free in pid_show+0x11f/0x13f
    [  263.888359] Read of size 4 at addr ffff8880bf0648c0 by task cat/746
    [  263.890479] CPU: 0 PID: 746 Comm: cat Not tainted 4.19.90-dirty #140
    [  263.893162] Call Trace:
    [  263.893509]  dump_stack+0x108/0x15f
    [  263.893999]  print_address_description+0xa5/0x372
    [  263.894641]  kasan_report.cold+0x236/0x2a8
    [  263.895696]  __asan_report_load4_noabort+0x25/0x30
    [  263.896365]  pid_show+0x11f/0x13f
    [  263.897422]  dev_attr_show+0x48/0x90
    [  263.898361]  sysfs_kf_seq_show+0x24d/0x4b0
    [  263.899479]  kernfs_seq_show+0x14e/0x1b0
    [  263.900029]  seq_read+0x43f/0x1150
    [  263.900499]  kernfs_fop_read+0xc7/0x5a0
    [  263.903764]  vfs_read+0x113/0x350
    [  263.904231]  ksys_read+0x103/0x270
    [  263.905230]  __x64_sys_read+0x77/0xc0
    [  263.906284]  do_syscall_64+0x106/0x360
    [  263.906797]  entry_SYSCALL_64_after_hwframe+0x44/0xa9
    
    Reproduce this issue as follows:
    1. nbd-server 8000 /tmp/disk
    2. nbd-client localhost 8000 /dev/nbd1
    3. cat /sys/block/nbd1/pid
    Then trigger use-after-free in pid_show.
    
    Reason is after do step '2', nbd-client progress is already exit. So
    it's task_struct already freed.
    To solve this issue, revert part of 6521d39a64b3's modify and remove
    useless 'recv_task' member of nbd_device.
    
    Fixes: 6521d39a64b3 ("nbd: Remove variable 'pid'")
    Signed-off-by: Ye Bin <yebin10@huawei.com>
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Link: https://lore.kernel.org/r/20211020073959.2679255-1-yebin10@huawei.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/nbd.c
+++ drivers/block/nbd.c
@@ -108,8 +108,9 @@ struct nbd_device {
 	struct gendisk *disk;
 
 	struct list_head list;
-	struct task_struct *task_recv;
 	struct task_struct *task_setup;
+
+	pid_t pid; /* pid of nbd-client, if attached */
 };
 
 #define NBD_CMD_REQUEUED	1
@@ -197,7 +198,7 @@ static ssize_t pid_show(struct device *dev,
 	struct gendisk *disk = dev_to_disk(dev);
 	struct nbd_device *nbd = (struct nbd_device *)disk->private_data;
 
-	return sprintf(buf, "%d\n", task_pid_nr(nbd->task_recv));
+	return sprintf(buf, "%d\n", nbd->pid);
 }
 
 static const struct device_attribute pid_attr = {
@@ -304,7 +305,7 @@ static void nbd_size_set(struct nbd_device *nbd, loff_t blocksize,
 	struct nbd_config *config = nbd->config;
 	config->blksize = blocksize;
 	config->bytesize = blocksize * nr_blocks;
-	if (nbd->task_recv != NULL)
+	if (!nbd->pid)
 		nbd_size_update(nbd);
 }
 
@@ -1113,7 +1114,7 @@ static void nbd_config_put(struct nbd_device *nbd)
 		if (test_and_clear_bit(NBD_HAS_PID_FILE,
 				       &config->runtime_flags))
 			device_remove_file(disk_to_dev(nbd->disk), &pid_attr);
-		nbd->task_recv = NULL;
+		nbd->pid = 0;
 		nbd_clear_sock(nbd);
 		if (config->num_connections) {
 			int i;
@@ -1144,7 +1145,7 @@ static int nbd_start_device(struct nbd_device *nbd)
 	int num_connections = config->num_connections;
 	int error = 0, i;
 
-	if (nbd->task_recv)
+	if (nbd->pid)
 		return -EBUSY;
 	if (!config->socks)
 		return -EINVAL;
@@ -1155,7 +1156,7 @@ static int nbd_start_device(struct nbd_device *nbd)
 	}
 
 	blk_mq_update_nr_hw_queues(&nbd->tag_set, config->num_connections);
-	nbd->task_recv = current;
+	nbd->pid = task_pid_nr(current);
 
 	nbd_parse_flags(nbd);
 
@@ -1398,8 +1399,8 @@ static int nbd_dbg_tasks_show(struct seq_file *s, void *unused)
 {
 	struct nbd_device *nbd = s->private;
 
-	if (nbd->task_recv)
-		seq_printf(s, "recv: %d\n", task_pid_nr(nbd->task_recv));
+	if (nbd->pid)
+		seq_printf(s, "recv: %d\n", nbd->pid);
 
 	return 0;
 }
@@ -1920,7 +1921,7 @@ static int nbd_genl_reconfigure(struct sk_buff *skb, struct genl_info *info)
 	mutex_lock(&nbd->config_lock);
 	config = nbd->config;
 	if (!test_bit(NBD_BOUND, &config->runtime_flags) ||
-	    !nbd->task_recv) {
+	    !nbd->pid) {
 		dev_err(nbd_to_dev(nbd),
 			"not configured, cannot reconfigure\n");
 		ret = -EINVAL;
