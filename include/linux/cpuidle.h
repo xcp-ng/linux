92ddb9abdbf0cb92e956fa5f8e8062e71973aa59
Corentin Oparowski <corentin.oparowski@vates.tech>
2024-04-11 10:18:42 +0000

    skip cpuidle driver init if cpuidle function disabled
    When kernel runs as Xen PV dom0, it does not have access to the required MSRs
    and so cpuidle is explicitly disabled in xen_arch_setup(). Skip intel_idle
    initialization early if cpuidle is disabled to avoid spurious warnings reading
    MSRs.
    
    [   11.706584]  <TASK>
    [   11.706629]  ? ex_handler_msr+0x128/0x140
    [   11.706716]  ? fixup_exception+0x85/0x340
    [   11.706803]  ? exc_general_protection+0x136/0x400
    [   11.706907]  ? asm_exc_general_protection+0x22/0x30
    [   11.707055]  ? xen_do_read_msr+0x81/0xa0
    [   11.707182]  xen_read_msr+0x20/0x30
    [   11.707340]  intel_idle_init+0xa5c/0xc90
    [   11.707642]  ? _raw_spin_unlock_irqrestore+0x18/0x20
    [   11.708159]  ? __pfx_intel_idle_init+0x10/0x10
    [   11.708451]  do_one_initcall+0x41/0x2e0
    [   11.708580]  kernel_init_freeable+0x1c0/0x2c0
    [   11.708748]  ? __pfx_kernel_init+0x10/0x10
    [   11.709013]  kernel_init+0x16/0x1b0
    [   11.709193]  ret_from_fork+0x2d/0x50
    [   11.709389]  ? __pfx_kernel_init+0x10/0x10
    [   11.709656]  ret_from_fork_asm+0x1b/0x30
    [   11.709844]  </TASK>
    
    Signed-off-by: Chunjie Zhu <chunjie.zhu@cloud.com>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -168,6 +168,7 @@ struct cpuidle_driver {
 };
 
 #ifdef CONFIG_CPU_IDLE
+extern int cpuidle_disabled(void);
 extern void disable_cpuidle(void);
 extern bool cpuidle_not_available(struct cpuidle_driver *drv,
 				  struct cpuidle_device *dev);
@@ -203,6 +204,8 @@ extern struct cpuidle_driver *cpuidle_get_cpu_driver(struct cpuidle_device *dev)
 static inline struct cpuidle_device *cpuidle_get_device(void)
 {return __this_cpu_read(cpuidle_devices); }
 #else
+static inline int cpuidle_disabled(void)
+{return 1; }
 static inline void disable_cpuidle(void) { }
 static inline bool cpuidle_not_available(struct cpuidle_driver *drv,
 					 struct cpuidle_device *dev)
