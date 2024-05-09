b3c34f5ee600a5c7469f55c875ebe634128da29e
Richard Gobert <richardbgobert@gmail.com>
2024-05-09 21:08:18 +0200

    net: gro: move L3 flush checks to tcp_gro_receive and udp_gro_receive_segment
    {inet,ipv6}_gro_receive functions perform flush checks (ttl, flags,
    iph->id, ...) against all packets in a loop. These flush checks are used in
    all merging UDP and TCP flows.
    
    These checks need to be done only once and only against the found p skb,
    since they only affect flush and not same_flow.
    
    This patch leverages correct network header offsets from the cb for both
    outer and inner network headers - allowing these checks to be done only
    once, in tcp_gro_receive and udp_gro_receive_segment. As a result,
    NAPI_GRO_CB(p)->flush is not used at all. In addition, flush_id checks are
    more declarative and contained in inet_gro_flush, thus removing the need
    for flush_id in napi_gro_cb.
    
    This results in less parsing code for non-loop flush tests for TCP and UDP
    flows.
    
    To make sure results are not within noise range - I've made netfilter drop
    all TCP packets, and measured CPU performance in GRO (in this case GRO is
    responsible for about 50% of the CPU utilization).
    
    perf top while replaying 64 parallel IP/TCP streams merging in GRO:
    (gro_receive_network_flush is compiled inline to tcp_gro_receive)
    net-next:
            6.94% [kernel] [k] inet_gro_receive
            3.02% [kernel] [k] tcp_gro_receive
    
    patch applied:
            4.27% [kernel] [k] tcp_gro_receive
            4.22% [kernel] [k] inet_gro_receive
    
    perf top while replaying 64 parallel IP/IP/TCP streams merging in GRO (same
    results for any encapsulation, in this case inet_gro_receive is top
    offender in net-next)
    net-next:
            10.09% [kernel] [k] inet_gro_receive
            2.08% [kernel] [k] tcp_gro_receive
    
    patch applied:
            6.97% [kernel] [k] inet_gro_receive
            3.68% [kernel] [k] tcp_gro_receive
    
    Signed-off-by: Richard Gobert <richardbgobert@gmail.com>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Link: https://lore.kernel.org/r/20240509190819.2985-3-richardbgobert@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/ipv4/tcp_offload.c
+++ net/ipv4/tcp_offload.c
@@ -238,9 +238,7 @@ struct sk_buff *tcp_gro_receive(struct list_head *head, struct sk_buff *skb)
 	goto out_check_final;
 
 found:
-	/* Include the IP ID check below from the inner most IP hdr */
-	flush = NAPI_GRO_CB(p)->flush;
-	flush |= (__force int)(flags & TCP_FLAG_CWR);
+	flush = (__force int)(flags & TCP_FLAG_CWR);
 	flush |= (__force int)((flags ^ tcp_flag_word(th2)) &
 		  ~(TCP_FLAG_CWR | TCP_FLAG_FIN | TCP_FLAG_PSH));
 	flush |= (__force int)(th->ack_seq ^ th2->ack_seq);
@@ -248,16 +246,7 @@ found:
 		flush |= *(u32 *)((u8 *)th + i) ^
 			 *(u32 *)((u8 *)th2 + i);
 
-	/* When we receive our second frame we can made a decision on if we
-	 * continue this flow as an atomic flow with a fixed ID or if we use
-	 * an incrementing ID.
-	 */
-	if (NAPI_GRO_CB(p)->flush_id != 1 ||
-	    NAPI_GRO_CB(p)->count != 1 ||
-	    !NAPI_GRO_CB(p)->is_atomic)
-		flush |= NAPI_GRO_CB(p)->flush_id;
-	else
-		NAPI_GRO_CB(p)->is_atomic = false;
+	flush |= gro_receive_network_flush(th, th2, p);
 
 	mss = skb_shinfo(p)->gso_size;
 
@@ -341,10 +330,8 @@ INDIRECT_CALLABLE_SCOPE int tcp4_gro_complete(struct sk_buff *skb, int thoff)
 
 	th->check = ~tcp_v4_check(skb->len - thoff, iph->saddr,
 				  iph->daddr, 0);
-	skb_shinfo(skb)->gso_type |= SKB_GSO_TCPV4;
-
-	if (NAPI_GRO_CB(skb)->is_atomic)
-		skb_shinfo(skb)->gso_type |= SKB_GSO_TCP_FIXEDID;
+	skb_shinfo(skb)->gso_type |= SKB_GSO_TCPV4 |
+			(NAPI_GRO_CB(skb)->ip_fixedid * SKB_GSO_TCP_FIXEDID);
 
 	tcp_gro_complete(skb);
 	return 0;
