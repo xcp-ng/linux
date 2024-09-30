dfafb1a080b98a12216cebb3b0053e4b576210c2
Joe Damato <jdamato@fastly.com>
2024-09-30 21:07:08 +0000

    gve: Map NAPI instances to queues
    Use the netdev-genl interface to map NAPI instances to queues so that
    this information is accessible to user programs via netlink.
    
    $ ./tools/net/ynl/cli.py --spec Documentation/netlink/specs/netdev.yaml \
    			 --dump queue-get --json='{"ifindex": 2}'
    
    [{'id': 0, 'ifindex': 2, 'napi-id': 8313, 'type': 'rx'},
     {'id': 1, 'ifindex': 2, 'napi-id': 8314, 'type': 'rx'},
     {'id': 2, 'ifindex': 2, 'napi-id': 8315, 'type': 'rx'},
     {'id': 3, 'ifindex': 2, 'napi-id': 8316, 'type': 'rx'},
     {'id': 4, 'ifindex': 2, 'napi-id': 8317, 'type': 'rx'},
    [...]
     {'id': 0, 'ifindex': 2, 'napi-id': 8297, 'type': 'tx'},
     {'id': 1, 'ifindex': 2, 'napi-id': 8298, 'type': 'tx'},
     {'id': 2, 'ifindex': 2, 'napi-id': 8299, 'type': 'tx'},
     {'id': 3, 'ifindex': 2, 'napi-id': 8300, 'type': 'tx'},
     {'id': 4, 'ifindex': 2, 'napi-id': 8301, 'type': 'tx'},
    [...]
    
    Signed-off-by: Joe Damato <jdamato@fastly.com>
    Reviewed-by: Praveen Kaligineedi <pkaligineedi@google.com>
    Link: https://patch.msgid.link/20240930210731.1629-3-jdamato@fastly.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 38667152
    
    (cherry picked from commit 021f9e671e4a7166e7a267531553b9e0b7d19681)
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- drivers/net/ethernet/google/gve/gve_main.c
+++ drivers/net/ethernet/google/gve/gve_main.c
@@ -1881,6 +1881,11 @@ static void gve_turndown(struct gve_priv *priv)
 
 		if (!gve_tx_was_added_to_block(priv, idx))
 			continue;
+
+		if (idx < priv->tx_cfg.num_queues)
+			netif_queue_set_napi(priv->dev, idx,
+					     NETDEV_QUEUE_TYPE_TX, NULL);
+
 		napi_disable(&block->napi);
 	}
 	for (idx = 0; idx < priv->rx_cfg.num_queues; idx++) {
@@ -1889,6 +1894,9 @@ static void gve_turndown(struct gve_priv *priv)
 
 		if (!gve_rx_was_added_to_block(priv, idx))
 			continue;
+
+		netif_queue_set_napi(priv->dev, idx, NETDEV_QUEUE_TYPE_RX,
+				     NULL);
 		napi_disable(&block->napi);
 	}
 
@@ -1920,6 +1928,12 @@ static void gve_turnup(struct gve_priv *priv)
 			continue;
 
 		napi_enable(&block->napi);
+
+		if (idx < priv->tx_cfg.num_queues)
+			netif_queue_set_napi(priv->dev, idx,
+					     NETDEV_QUEUE_TYPE_TX,
+					     &block->napi);
+
 		if (gve_is_gqi(priv)) {
 			iowrite32be(0, gve_irq_doorbell(priv, block));
 		} else {
@@ -1942,6 +1956,9 @@ static void gve_turnup(struct gve_priv *priv)
 			continue;
 
 		napi_enable(&block->napi);
+		netif_queue_set_napi(priv->dev, idx, NETDEV_QUEUE_TYPE_RX,
+				     &block->napi);
+
 		if (gve_is_gqi(priv)) {
 			iowrite32be(0, gve_irq_doorbell(priv, block));
 		} else {
