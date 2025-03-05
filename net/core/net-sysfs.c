6aabfc30b10975768f27010c009d0818251684d0
Stanislav Fomichev <sdf@fomichev.me>
2025-03-05 08:37:26 -0800

    net: hold netdev instance lock during sysfs operations
    Most of them are already covered by the converted dev_xxx APIs.
    Add the locking wrappers for the remaining ones.
    
    Cc: Saeed Mahameed <saeed@kernel.org>
    Signed-off-by: Stanislav Fomichev <sdf@fomichev.me>
    Link: https://patch.msgid.link/20250305163732.2766420-9-sdf@fomichev.me
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 38667152
    
    (cherry picked from commit ad7c7b2172c388818a111455643491d75f535e90)
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -1370,8 +1370,10 @@ static ssize_t tx_maxrate_store(struct netdev_queue *queue,
 		return restart_syscall();
 
 	err = -EOPNOTSUPP;
+	netdev_lock_ops(dev);
 	if (dev->netdev_ops->ndo_set_tx_maxrate)
 		err = dev->netdev_ops->ndo_set_tx_maxrate(dev, index, rate);
+	netdev_unlock_ops(dev);
 
 	rtnl_unlock();
 	if (!err) {
