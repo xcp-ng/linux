87730fae2e1dfa81f60f9599b485a7f248d3fed0
Mina Almasry <almasrymina@google.com>
2025-05-08 00:48:21 +0000

    netmem: add niov->type attribute to distinguish different net_iov types
    Later patches in the series adds TX net_iovs where there is no pp
    associated, so we can't rely on niov->pp->mp_ops to tell what is the
    type of the net_iov.
    
    Add a type enum to the net_iov which tells us the net_iov type.
    
    Signed-off-by: Mina Almasry <almasrymina@google.com>
    Link: https://patch.msgid.link/20250508004830.4100853-2-almasrymina@google.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    
    Orabug: 39203117
    
    (cherry picked from commit 03e96b8c11d140fb4ead0b30c2d6e1a294b501ef)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	io_uring/zcrx.c
    
    Skip changes in this file, since it is not part of UEK8 as of now.
    
    Conflicts:
    	net/core/devmem.c
    
    Skip the changes since the corresponding code was not backported.
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>

--- net/core/devmem.c
+++ net/core/devmem.c
@@ -300,6 +300,7 @@ net_devmem_bind_dmabuf(struct net_device *dev, unsigned int dmabuf_fd,
 
 		for (i = 0; i < owner->area.num_niovs; i++) {
 			niov = &owner->area.niovs[i];
+			niov->type = NET_IOV_DMABUF;
 			niov->owner = &owner->area;
 			page_pool_set_dma_addr_netmem(net_iov_to_netmem(niov),
 						      net_devmem_get_dma_addr(niov));
