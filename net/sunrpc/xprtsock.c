2918cd6c416ff8be8ce58089afd37b3554a35fe1
Chunjie Zhu <chunjie.zhu@cloud.com>
2025-06-19 04:44:24 +0000

    SUNRPC: Replace the queue timer with a delayed work function
      The queue timer function, which walks the RPC queue in order to locate
      candidates for waking up is one of the current constraints against
      removing the bh-safe queue spin locks. Replace it with a delayed
      work queue, so that we can do the actual rpc task wake ups from an
      ordinary process context.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- net/sunrpc/xprtsock.c
+++ net/sunrpc/xprtsock.c
@@ -1610,6 +1610,7 @@ static void xs_tcp_state_change(struct sock *sk)
 	trace_rpc_socket_state_change(xprt, sk->sk_socket);
 	switch (sk->sk_state) {
 	case TCP_ESTABLISHED:
+		spin_lock(&xprt->transport_lock);
 		if (!xprt_test_and_set_connected(xprt)) {
 
 			/* Reset TCP record info */
@@ -1624,6 +1625,7 @@ static void xs_tcp_state_change(struct sock *sk)
 
 			xs_run_error_worker(transport, XPRT_SOCK_WAKE_PENDING);
 		}
+		spin_unlock(&xprt->transport_lock);
 		break;
 	case TCP_FIN_WAIT1:
 		/* The client initiated a shutdown of the socket */
