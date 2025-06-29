5dd5a370e5858663e03fafbd771f44afe707baf3
Carolina Jubran <cjubran@nvidia.com>
2025-06-29 17:21:32 +0300

    devlink: Extend devlink rate API with traffic classes bandwidth management
    Introduce support for specifying relative bandwidth shares between
    traffic classes (TC) in the devlink-rate API. This new option allows
    users to allocate bandwidth across multiple traffic classes in a
    single command.
    
    This feature provides a more granular control over traffic management,
    especially for scenarios requiring Enhanced Transmission Selection.
    
    Users can now define a relative bandwidth share for each traffic class.
    For example, assigning share values of 20 to TC0 (TCP/UDP) and 80 to TC5
    (RoCE) will result in TC0 receiving 20% and TC5 receiving 80% of the
    total bandwidth. The actual percentage each class receives depends on
    the ratio of its share value to the sum of all shares.
    
    Example:
    DEV=pci/0000:08:00.0
    
    $ devlink port function rate add $DEV/vfs_group tx_share 10Gbit \
      tx_max 50Gbit tc-bw 0:20 1:0 2:0 3:0 4:0 5:80 6:0 7:0
    
    $ devlink port function rate set $DEV/vfs_group \
      tc-bw 0:20 1:0 2:0 3:0 4:0 5:20 6:60 7:0
    
    Example usage with ynl:
    
    ./tools/net/ynl/cli.py --spec Documentation/netlink/specs/devlink.yaml \
      --do rate-set --json '{
      "bus-name": "pci",
      "dev-name": "0000:08:00.0",
      "port-index": 1,
      "rate-tc-bws": [
        {"rate-tc-index": 0, "rate-tc-bw": 50},
        {"rate-tc-index": 1, "rate-tc-bw": 50},
        {"rate-tc-index": 2, "rate-tc-bw": 0},
        {"rate-tc-index": 3, "rate-tc-bw": 0},
        {"rate-tc-index": 4, "rate-tc-bw": 0},
        {"rate-tc-index": 5, "rate-tc-bw": 0},
        {"rate-tc-index": 6, "rate-tc-bw": 0},
        {"rate-tc-index": 7, "rate-tc-bw": 0}
      ]
    }'
    
    ./tools/net/ynl/cli.py --spec Documentation/netlink/specs/devlink.yaml \
      --do rate-get --json '{
      "bus-name": "pci",
      "dev-name": "0000:08:00.0",
      "port-index": 1
    }'
    
    output for rate-get:
    {'bus-name': 'pci',
     'dev-name': '0000:08:00.0',
     'port-index': 1,
     'rate-tc-bws': [{'rate-tc-bw': 50, 'rate-tc-index': 0},
                     {'rate-tc-bw': 50, 'rate-tc-index': 1},
                     {'rate-tc-bw': 0, 'rate-tc-index': 2},
                     {'rate-tc-bw': 0, 'rate-tc-index': 3},
                     {'rate-tc-bw': 0, 'rate-tc-index': 4},
                     {'rate-tc-bw': 0, 'rate-tc-index': 5},
                     {'rate-tc-bw': 0, 'rate-tc-index': 6},
                     {'rate-tc-bw': 0, 'rate-tc-index': 7}],
     'rate-tx-max': 0,
     'rate-tx-priority': 0,
     'rate-tx-share': 0,
     'rate-tx-weight': 0,
     'rate-type': 'leaf'}
    
    Signed-off-by: Carolina Jubran <cjubran@nvidia.com>
    Reviewed-by: Cosmin Ratiu <cratiu@nvidia.com>
    Reviewed-by: Jiri Pirko <jiri@nvidia.com>
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>
    Signed-off-by: Mark Bloch <mbloch@nvidia.com>
    Link: https://patch.msgid.link/20250629142138.361537-3-mbloch@nvidia.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 39203117
    
    (cherry picked from commit 566e8f108fc7847f2a8676ec6a101d37b7dd0fb4)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	Documentation/netlink/specs/devlink.yaml
    	include/uapi/linux/devlink.h
    
    Conflicts because commit da0e2197645c8 ("devlink: Make health reporter
    burst period configurable") is already backported, and it has added
    DEVLINK_ATTR_HEALTH_REPORTER_BURST_PERIOD in enum devlink_attr. To
    not break the ABI, the order in the enum shouldn't be changed so
    add the new values (DEVLINK_ATTR_RATE_TC_*) after
    DEVLINK_ATTR_HEALTH_REPORTER_BURST_PERIOD. Do the same in
    devlink.yaml and add definitions of rate-tc-* after
    health-reporter-burst-period.
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Reviewed-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>

--- include/uapi/linux/devlink.h
+++ include/uapi/linux/devlink.h
@@ -221,6 +221,11 @@ enum devlink_port_flavour {
 				      */
 };
 
+/* IEEE 802.1Qaz standard supported values. */
+
+#define DEVLINK_RATE_TCS_MAX 8
+#define DEVLINK_RATE_TC_INDEX_MAX (DEVLINK_RATE_TCS_MAX - 1)
+
 enum devlink_rate_type {
 	DEVLINK_RATE_TYPE_LEAF,
 	DEVLINK_RATE_TYPE_NODE,
@@ -630,6 +635,10 @@ enum devlink_attr {
 	DEVLINK_ATTR_REGION_DIRECT,		/* flag */
 
 	DEVLINK_ATTR_HEALTH_REPORTER_BURST_PERIOD,	/* u64 */
+
+	DEVLINK_ATTR_RATE_TC_BWS,		/* nested */
+	DEVLINK_ATTR_RATE_TC_INDEX,		/* u8 */
+	DEVLINK_ATTR_RATE_TC_BW,		/* u32 */
 
 	/* Add new attributes above here, update the spec in
 	 * Documentation/netlink/specs/devlink.yaml and re-generate
