fb1bdf7c9acbc595edef582f6c3ded23f1f235b8
Byungchul Park <byungchul@sk.com>
2025-07-21 11:18:27 +0900

    netdevsim: access ->pp through netmem_desc instead of page
    To eliminate the use of struct page in page pool, the page pool users
    should use netmem descriptor and APIs instead.
    
    Make netdevsim access ->pp through netmem_desc instead of page.
    
    Signed-off-by: Byungchul Park <byungchul@sk.com>
    Link: https://patch.msgid.link/20250721021835.63939-5-byungchul@sk.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 39203117
    
    (cherry picked from commit 6fd824342a57164d97717325763daee1ef01cbcd)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: 6fd824342a57164d97717325763daee1ef01cbcd
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>

--- drivers/net/netdevsim/netdev.c
+++ drivers/net/netdevsim/netdev.c
@@ -926,7 +926,8 @@ nsim_pp_hold_write(struct file *file, const char __user *data,
 		if (!ns->page)
 			ret = -ENOMEM;
 	} else {
-		page_pool_put_full_page(ns->page->pp, ns->page, false);
+		page_pool_put_full_page(pp_page_to_nmdesc(ns->page)->pp,
+					ns->page, false);
 		ns->page = NULL;
 	}
 
@@ -1141,7 +1142,8 @@ void nsim_destroy(struct netdevsim *ns)
 
 	/* Put this intentionally late to exercise the orphaning path */
 	if (ns->page) {
-		page_pool_put_full_page(ns->page->pp, ns->page, false);
+		page_pool_put_full_page(pp_page_to_nmdesc(ns->page)->pp,
+					ns->page, false);
 		ns->page = NULL;
 	}
 
