ad3c86b13b9fae894a70bb2572d36313edd7ea24
Teddy Astie <teddy.astie@vates.tech>
2025-10-21 10:08:45 +0200

    amd: Skip print_s5_reset_status_mmio as a guest
    This is incorrect as a guest and causes issues with SEV-ES.
    
    Signed-off-by: Teddy Astie <teddy.astie@vates.tech>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -1328,7 +1328,7 @@ static __init int print_s5_reset_status_mmio(void)
 	void __iomem *addr;
 	int i;
 
-	if (!cpu_feature_enabled(X86_FEATURE_ZEN))
+	if (!cpu_feature_enabled(X86_FEATURE_ZEN) || cpu_feature_enabled(X86_FEATURE_HYPERVISOR))
 		return 0;
 
 	addr = ioremap(FCH_PM_BASE + FCH_PM_S5_RESET_STATUS, sizeof(value));
