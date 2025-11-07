c3744ae462ac56a13dd5c5d360c9b626f99a0e5a
Quentin Casasnovas <quentin.casasnovas@vates.tech>
2025-11-07 15:28:19 +0100

    xen-ioemu-inject-msi
    Signed-off-by: Quentin Casasnovas <quentin.casasnovas@vates.tech>

---
+++ include/xen/ioemu.h
@@ -0,0 +1,35 @@
+/*
+ * Helpers for in-kernel device emulators.
+ *
+ * Copyright (C) 2014 Citrix Systems UK Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation; or, when distributed
+ * separately from the Linux kernel or incorporated into other
+ * software packages, subject to the following license:
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this source file (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#ifndef _XEN_IOEMU_H
+#define _XEN_IOEMU_H
+
+int xen_ioemu_inject_msi(domid_t domid, uint64_t addr, uint32_t data);
+
+#endif /* #ifndef _XEN_IOEMU_H */
