abea63eb9d47bd7b4b451509ce37ba19556b1449
Prakash Sangappa <prakash.sangappa@oracle.com>
2026-03-13 06:27:19 +0000

    Revert "Scheduler extension change under Oracle Extensions and modify enum value"
    This reverts commit e0b597468fb803a88b41dbe624253618719336a5.
    
    Orabug: 39047408
    
    Signed-off-by: Prakash Sangappa <prakash.sangappa@oracle.com>
    Reviewed-by: Joseph Salisbury <joseph.salisbury@oracle.com>
    Reviewed-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>

--- kernel/rseq.c
+++ kernel/rseq.c
@@ -361,7 +361,6 @@ error:
 	force_sigsegv(sig);
 }
 
-#ifndef WITHOUT_ORACLE_EXTENSIONS
 bool rseq_delay_resched(void)
 {
 	struct task_struct *t = current;
@@ -418,7 +417,6 @@ void rseq_delay_resched_tick(void)
 		set_tsk_need_resched(t);
 #endif
 }
-#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 #ifdef CONFIG_DEBUG_RSEQ
 
