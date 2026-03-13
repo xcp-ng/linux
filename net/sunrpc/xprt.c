d898f4707efb8579917bc8865cb9a61fcbdf01d9
Frediano Ziglio <frediano.ziglio@citrix.com>
2026-03-13 17:00:15 +0100

    XSI-2150: Avoid deadlocks on NFS spinlocks
    Due to some backport trying to fix a possible deadlock (see CA-399062
    for details) some patches which introduced many more deadlock were
    merged to XS8.
    Revert these patches and fix the original potential deadlock too.
    
    These deadlock were generated by the same lock taken by no-IRQ context
    and nested lock attempt on IRQ context as IRQ were not disabled on
    first lock. Calling spin_lock_bh instead of spin_lock prevent IRQ
    code nested to no-IRQ code.
    
    The removal of "transport_lock" in "xs_tcp_state_change" is enabled by
    commit 13f6175 ("XSI-2150: Remove added chunk causing deadlock",
    specifically "SUNRPC: Move reset of TCP state variables into the
    reconnect code"). A review of "xs_tcp_state_change" shows that similar
    accesses through the "transport" pointer (notably for "TCP_CLOSE_WAIT"
    and "TCP_CLOSE") are already performed without this lock. State bit
    updates are atomic, and the work queue is adequately protected by
    other synchronization mechanisms, including additional spinlocks and
    CPU-local variables.

--- net/sunrpc/xprt.c
+++ net/sunrpc/xprt.c
@@ -765,18 +765,18 @@ xprt_init_autodisconnect(struct timer_list *t)
 {
 	struct rpc_xprt *xprt = from_timer(xprt, t, timer);
 
-	spin_lock(&xprt->transport_lock);
+	spin_lock_bh(&xprt->transport_lock);
 	if (!list_empty(&xprt->recv))
 		goto out_abort;
 	/* Reset xprt->last_used to avoid connect/autodisconnect cycling */
 	xprt->last_used = jiffies;
 	if (test_and_set_bit(XPRT_LOCKED, &xprt->state))
 		goto out_abort;
-	spin_unlock(&xprt->transport_lock);
+	spin_unlock_bh(&xprt->transport_lock);
 	queue_work(xprtiod_workqueue, &xprt->task_cleanup);
 	return;
 out_abort:
-	spin_unlock(&xprt->transport_lock);
+	spin_unlock_bh(&xprt->transport_lock);
 }
 
 bool xprt_lock_connect(struct rpc_xprt *xprt,
