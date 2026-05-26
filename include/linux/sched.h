26ba6639ed3fc9646cecbbd2d6a2d14795847425
Quentin Casasnovas <quentin.casasnovas@vates.tech>
2026-05-26 11:18:00 +0200

    !kabi ptrace: slightly saner 'get_dumpable()' logic
    This work-arounds kABI changes introduced by the user_dumpable field
    addition to the struct task_struct.  It's easy enough to move below to a
    hole and hide it from __GENKSYMS__.
    
    Fixes: 7f53dd12fb0b ("ptrace: slightly saner 'get_dumpable()' logic")
    Signed-off-by: Quentin Casasnovas <quentin.casasnovas@vates.tech>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -715,9 +715,6 @@ struct task_struct {
 
 	/* Unserialized, strictly 'current' */
 
-	/* Save user-dumpable when mm goes away */
-	unsigned			user_dumpable:1;
-
 	/* Bit to tell LSMs we're in execve(): */
 	unsigned			in_execve:1;
 	unsigned			in_iowait:1;
@@ -740,6 +737,16 @@ struct task_struct {
 #ifdef CONFIG_BLK_CGROUP
 	/* to be used once the psi infrastructure lands upstream. */
 	unsigned			use_memdelay:1;
+#endif
+#ifndef __GENKSYMS__
+	/**
+	 * 7f53dd12fb0b ("ptrace: slightly saner 'get_dumpable()' logic")
+	 * added the user_dumpable field, so hide it from genksyms to avoid
+	 * kABI changes.  Also move it inside a hole to avoid changing
+	 * offsets of other fields.
+	 */
+	/* Save user-dumpable when mm goes away */
+	unsigned			user_dumpable:1;
 #endif
 
 	unsigned long			atomic_flags; /* Flags requiring atomic access. */
