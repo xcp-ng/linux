12134c14b4f59933c032387f0e94b92f8d82ff16
Jane Chu <jane.chu@oracle.com>
2026-06-26 16:28:24 -0600

    mm/huge_memory: add split_huge_page_to_order()
    Patch series "Optimize folio split in memory failure", v5.
    
    This patchset optimizes folio split operations in memory failure code by
    always splitting a folio to min_order_for_split() to minimize unusable
    pages, even if min_order_for_split() is non zero and memory failure code
    would take the failed path eventually for a successfully split folio.
    
    This means instead of making the entire original folio unusable memory
    failure code would only make its after-split folio, which has order of
    min_order_for_split() and contains HWPoison page, unusable.
    
    For soft offline case, since the original folio is still accessible, no
    split is performed if the folio cannot be split to order-0 to prevent
    potential performance loss.
    
    In addition, add split_huge_page_to_order() to improve code readability
    and fix kernel-doc comment format for folio_split() and other related
    functions.
    
    Background
    ==========
    
    This patchset is a follow-up of "[PATCH v3] mm/huge_memory: do not change
    split_huge_page*() target order silently."[1] and [PATCH v4]
    mm/huge_memory: preserve PG_has_hwpoisoned if a folio is split to >0
    order[2], since both are separated out as hotfixes.  It improves how
    memory failure code handles large block size(LBS) folios with
    min_order_for_split() > 0.  By splitting a large folio containing HW
    poisoned pages to min_order_for_split(), the after-split folios without HW
    poisoned pages could be freed for reuse.  To achieve this, folio split
    code needs to set has_hwpoisoned on after-split folios containing HW
    poisoned pages and it is done in the hotfix in [2].
    
    This patchset includes:
    1. A patch adds split_huge_page_to_order(),
    2. Patch 2 and Patch 3 of "[PATCH v2 0/3] Do not change split folio target
       order"[3],
    
    This patch (of 3):
    
    When the caller does not supply a list to
    split_huge_page_to_list_to_order(), use split_huge_page_to_order()
    instead.
    
    Link: https://lkml.kernel.org/r/20251031162001.670503-1-ziy@nvidia.com
    Link: https://lkml.kernel.org/r/20251031162001.670503-2-ziy@nvidia.com
    Link: https://lore.kernel.org/all/20251017013630.139907-1-ziy@nvidia.com/ [1]
    Link: https://lore.kernel.org/all/20251023030521.473097-1-ziy@nvidia.com/ [2]
    Link: https://lore.kernel.org/all/20251016033452.125479-1-ziy@nvidia.com/ [3]
    Signed-off-by: Zi Yan <ziy@nvidia.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Reviewed-by: Wei Yang <richard.weiyang@gmail.com>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Barry Song <baohua@kernel.org>
    Reviewed-by: Lance Yang <lance.yang@linux.dev>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Dev Jain <dev.jain@arm.com>
    Cc: Jane Chu <jane.chu@oracle.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Luis Chamberalin <mcgrof@kernel.org>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Naoya Horiguchi <nao.horiguchi@gmail.com>
    Cc: Nico Pache <npache@redhat.com>
    Cc: Pankaj Raghav <kernel@pankajraghav.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Yang Shi <shy828301@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit a7ef12c64fd991c0f42b2e1bf0c4f09068575864)
    
    Conflicts:
            include/linux/huge_mm.h
    due to missing 7460b470a131f ("mm/truncate: use folio_split() in truncate operation")
    and several other commits that aren't essential to this commit but
    are messy to backport. Resolve by ignoring the non-essential code.
    
    Orabug: 39215509
    
    Signed-off-by: Jane Chu <jane.chu@oracle.com>
    Reviewed-by: Harry Yoo <harry.yoo@oracle.com>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -359,6 +359,10 @@ int split_huge_page_to_list_to_order(struct page *page, struct list_head *list,
 		unsigned int new_order);
 int min_order_for_split(struct folio *folio);
 int split_folio_to_list(struct folio *folio, struct list_head *list);
+static inline int split_huge_page_to_order(struct page *page, unsigned int new_order)
+{
+	return split_huge_page_to_list_to_order(page, NULL, new_order);
+}
 static inline int split_huge_page(struct page *page)
 {
 	return split_huge_page_to_list_to_order(page, NULL, 0);
@@ -527,6 +531,11 @@ split_huge_page_to_list_to_order(struct page *page, struct list_head *list,
 		unsigned int new_order)
 {
 	return 0;
+}
+static inline int split_huge_page_to_order(struct page *page, unsigned int new_order)
+{
+	VM_WARN_ON_ONCE_PAGE(1, page);
+	return -EINVAL;
 }
 static inline int split_huge_page(struct page *page)
 {
