oracle/linux-uek

commit 70b41b4bbbfea07b471d512d6f12f747c493fa74

mm: Allow userspace to reserve VA range for use by userspace only
Add support for ELF binaries to reserve address ranges. Address
range can be reserved at load time by adding an ELF NOTE section, or
at run time with mprotect() with PROT_RESERVED flag. Reserved ranges
can be allocated with mmap(..... MAP_FIXED...) and shmat(....,
SHM_REMAP) later. Any reserved address ranges are annotated with
"[rsvd]" in /proc/<pid>/maps output. A binary can check if the
kernel supports VA range reservation by checking the value of
auxiliary vector AT_VA_RESERVATION.

VA reservation is done by adding a special NOTE section to binary
using declarations similar to following:

        .section .note.rsvd_range, "a", @note
        .p2align 2
        .long 1f - 0f           # name size (not including padding)
        .long 3f - 2f           # desc size (not including padding)
        .long 0x07c10001
0:      .asciz "Reserved VA"        # name
1:      .p2align 2
2:      .quad 0x7f2000000000
        .quad 0x7f2000e00000
        .quad 0x7f5000200000
        .quad 0x7f500d000000
3:  .p2align 2

Each reserved range is specified as pair of addresses (start and
end).  This note section is read by kernel elf loader and address
ranges are reserved for the lifetime of process. A maximum of 64
such entries can be made in NOTE section. Execution of a binary file
with more than 64 pairs of addresses in this note section will be
terminated with ENOEXEC.

NOTE: Kernel can not guarantee all VA ranges in the NOTE section
will be reserved. If the address range is valid but is already in
use (possibly by a shared library loaded earlier), execution of
binary will be terminated with ENOMEM.

NOTE: Due to limitations in the implementation, when mapping a
range over over one or more reserved ranges the range must be
entirely contained within a reserved range or a contiguous set of
reserved ranges. mmap() will fail and set errno to EINVAL if
the range to map is only partly reserved.

fs/binfmt_elf.c
@@ -90,6 +90,8 @@ static int elf_core_dump(struct coredump_params *cprm);
 #define ELF_MIN_ALIGN	PAGE_SIZE
 #endif
 
+#define MAX_FILE_NOTE_SIZE (4*1024*1024)
+
 #ifndef ELF_CORE_EFLAGS
 #define ELF_CORE_EFLAGS	0
 #endif
@@ -274,6 +276,7 @@ create_elf_tables(struct linux_binprm *bprm, const struct elfhdr *exec,
 	NEW_AUX_ENT(AT_RSEQ_FEATURE_SIZE, offsetof(struct rseq, end));
 	NEW_AUX_ENT(AT_RSEQ_ALIGN, __alignof__(struct rseq));
 #endif
+	NEW_AUX_ENT(AT_VA_RESERVATION, 1);
 #undef NEW_AUX_ENT
 	/* AT_NULL is zero; clear the rest too */
 	memset(elf_info, 0, (char *)mm->saved_auxv +
@@ -816,6 +819,111 @@ static int parse_elf_properties(struct file *f, const struct elf_phdr *phdr,
 	return ret == -ENOENT ? 0 : ret;
 }
 
+#define MAX_RSVD_VA_RANGES	64
+#define RSVD_VA_STRING		"Reserved VA"
+#define SZ_RSVD_VA_STRING	sizeof(RSVD_VA_STRING)
+
+static int reserve_va_range(struct elf_phdr *elf_ppnt,
+				struct linux_binprm *bprm)
+{
+	char *note_seg = NULL;
+	struct elf_note *note;
+	loff_t pos = elf_ppnt->p_offset;
+	int retval = 0;
+	size_t note_size = elf_ppnt->p_filesz;
+
+	note_seg = kvmalloc(note_size, GFP_KERNEL);
+	if (!note_seg) {
+		retval = -ENOMEM;
+		return retval;
+	}
+
+	retval = kernel_read(bprm->file, note_seg, note_size, &pos);
+	if (retval != note_size) {
+		if (retval >= 0)
+			retval = -EIO;
+		goto out;
+	}
+
+	note = (struct elf_note *)note_seg;
+	while ((char *)note + sizeof(struct elf_note) <
+	       (char *)(note_seg + note_size)) {
+		char *name;
+		unsigned long *val;
+		unsigned long nentry, i;
+
+		if (note->n_type != 0x07c10001)
+			goto cont_loop;
+
+		/* Sanity check for malformed note entry */
+		if (note->n_namesz > SZ_RSVD_VA_STRING) {
+			retval = -ENOEXEC;
+			goto out;
+		}
+
+		name = (char *)note + sizeof(struct elf_note);
+		if (strncmp(name, RSVD_VA_STRING, SZ_RSVD_VA_STRING) == 0) {
+			nentry = note->n_descsz/sizeof(void *);
+			val = (unsigned long *)(name +
+						roundup(note->n_namesz, 4));
+			/*
+			 * Check if right number of address
+			 * entries exist in note section
+			 */
+			if (((nentry % 2) != 0) ||
+				((nentry / 2) > MAX_RSVD_VA_RANGES)) {
+				retval = -ENOEXEC;
+				goto out;
+			}
+			for (i = 0 ; i < nentry; i += 2) {
+				unsigned long range1, range2, size;
+				struct mm_struct *mm = current->mm;
+
+				/*
+				 * Ensure we can access two address entries
+				 * in this note segment safely
+				 */
+				if ((char *)(val + 1) >=
+					((char *)note_seg + note_size)) {
+					retval = -ENOEXEC;
+					goto out;
+				}
+				range1 = PAGE_ALIGN((*val++) - PAGE_SIZE + 1);
+				range2 = PAGE_ALIGN(*val++);
+				size = range2 - range1;
+
+				/* Validate the address range being reserved */
+				if ((range2 <= range1) ||
+				    (!access_ok((void *)range1, size))) {
+					retval = -ENOEXEC;
+					goto out;
+				}
+
+				/*
+				 * install_rsvd_mapping() requires the mmap
+				 * write lock be held by the caller. Needed vma
+				 * write locks will be taken later.
+				 */
+				mmap_write_lock(mm);
+				retval = install_rsvd_mapping(mm, range1, size);
+				mmap_write_unlock(mm);
+
+				if (retval < 0)
+					goto out;
+			}
+		}
+cont_loop:
+		note = (struct elf_note *)((char *)note +
+						sizeof(struct elf_note) +
+						roundup(note->n_namesz, 4) +
+						roundup(note->n_descsz, 4));
+	}
+
+out:
+	kvfree(note_seg);
+	return retval;
+}
+
 static int load_elf_binary(struct linux_binprm *bprm)
 {
 	struct file *interpreter = NULL; /* to shut gcc up */
@@ -1022,6 +1130,24 @@ out_free_interp:
 	end_code = 0;
 	start_data = 0;
 	end_data = 0;
+
+	/*
+	 * Read the notes segment to find notes to reserve address space
+	 */
+	elf_ppnt = elf_phdata;
+	for (i = 0; i < elf_ex->e_phnum; i++, elf_ppnt++) {
+		if (elf_ppnt->p_type == PT_NOTE) {
+			/* Malformed note segments are ignored */
+			if ((elf_ppnt->p_filesz > MAX_FILE_NOTE_SIZE) ||
+			    (elf_ppnt->p_filesz < sizeof(struct elf_note)))
+				continue;
+
+			retval = reserve_va_range(elf_ppnt, bprm);
+			if (retval < 0)
+				goto out_free_ph;
+		}
+	}
+
 
 	/* Now we do a little grungy work by mmapping the ELF image into
 	   the correct location in memory. */

include/linux/mm.h
@@ -428,6 +428,16 @@ extern unsigned int kobjsize(const void *objp);
 #endif
 
 #ifdef CONFIG_64BIT
+# define VM_RSVD_VA_BIT			48
+# define VM_RSVD_VA			BIT(VM_RSVD_VA_BIT) /* Reserved VA range */
+# define VM_RSVD_PLACEHOLDER_BIT	49
+# define VM_RSVD_PLACEHOLDER		BIT(VM_RSVD_PLACEHOLDER_BIT)
+#else
+# define VM_RSVD_VA			VM_NONE
+# define VM_RSVD_PLACEHOLDER		VM_NONE
+#endif
+
+#ifdef CONFIG_64BIT
 /* VM is sealed, in vm_flags */
 #define VM_SEALED	_BITUL(63)
 #endif
@@ -990,6 +1000,16 @@ static inline bool is_shared_maywrite(vm_flags_t vm_flags)
 static inline bool vma_is_shared_maywrite(struct vm_area_struct *vma)
 {
 	return is_shared_maywrite(vma->vm_flags);
+}
+
+static inline bool vma_is_rsvd_va(const struct vm_area_struct *vma)
+{
+	return vma->vm_flags & VM_RSVD_VA;
+}
+
+static inline bool vma_is_rsvd_placeholder(const struct vm_area_struct *vma)
+{
+	return vma->vm_flags & VM_RSVD_PLACEHOLDER;
 }
 
 static inline
@@ -3294,6 +3314,8 @@ void anon_vma_interval_tree_verify(struct anon_vma_chain *node);
 	     avc; avc = anon_vma_interval_tree_iter_next(avc, start, last))
 
 /* mmap.c */
+int install_rsvd_mapping(struct mm_struct *mm, unsigned long addr,
+			 unsigned long len);
 extern int __vm_enough_memory(struct mm_struct *mm, long pages, int cap_sys_admin);
 extern int insert_vm_struct(struct mm_struct *, struct vm_area_struct *);
 extern void exit_mmap(struct mm_struct *);

include/uapi/asm-generic/mman-common.h
@@ -16,6 +16,7 @@
 #define PROT_NONE	0x0		/* page can not be accessed */
 #define PROT_GROWSDOWN	0x01000000	/* mprotect flag: extend change to start of growsdown vma */
 #define PROT_GROWSUP	0x02000000	/* mprotect flag: extend change to end of growsup vma */
+#define PROT_RESERVED	0x10000000	/* Reserve this VA range */
 
 /* 0x01 - 0x03 are defined in linux/mman.h */
 #define MAP_TYPE	0x0f		/* Mask for type of mapping */

include/uapi/linux/auxvec.h
@@ -41,4 +41,6 @@
 #define AT_MINSIGSTKSZ	51	/* minimal stack size for signal delivery */
 #endif
 
+#define AT_VA_RESERVATION	61	/* VA reservation support */
+
 #endif /* _UAPI_LINUX_AUXVEC_H */

mm/mmap.c
@@ -1394,6 +1394,15 @@ static unsigned long __mmap_region(struct file *file, unsigned long addr,
 		vmg.next = vma_iter_next_rewind(&vmi, &vmg.prev);
 	}
 
+	if (vms.has_rsvd_vmas) {
+		if (vms.partly_spans_reserved) {
+			error = -EINVAL;
+			goto abort_munmap;
+		}
+		vm_flags |= VM_RSVD_VA;
+		vmg.flags = vm_flags;
+	}
+
 	/* Check against address space limit. */
 	if (!may_expand_vm(mm, vm_flags, pglen - vms.nr_pages)) {
 		error = -ENOMEM;

mm/mprotect.c
@@ -732,7 +732,11 @@ static int do_mprotect_pkey(unsigned long start, size_t len,
 	end = start + len;
 	if (end <= start)
 		return -ENOMEM;
-	if (!arch_validate_prot(prot, start))
+	/*
+	 * Remove PROT_RESERVED rather than teach architectures that
+	 * it is okay.
+	 */
+	if (!arch_validate_prot(prot & ~PROT_RESERVED, start))
 		return -EINVAL;
 
 	reqprot = prot;
@@ -751,6 +755,13 @@ static int do_mprotect_pkey(unsigned long start, size_t len,
 	vma_iter_init(&vmi, current->mm, start);
 	vma = vma_find(&vmi, end);
 	error = -ENOMEM;
+	if (prot & PROT_RESERVED) {
+		if (vma || !access_ok((const void *)start, len))
+			error = -EINVAL;
+		else
+			error = install_rsvd_mapping(current->mm, start, len);
+		goto out;
+	}
 	if (!vma)
 		goto out;
 

mm/vma.c
@@ -925,9 +925,19 @@ struct vm_area_struct *vma_merge_new_range(struct vma_merge_struct *vmg)
 
 	vmg->state = VMA_MERGE_NOMERGE;
 
-	/* Special VMAs are unmergeable, also if no prev/next. */
-	if ((vmg->flags & VM_SPECIAL) || (!prev && !next))
+	/* Nothing to merge. */
+	if (!prev && !next)
 		return NULL;
+
+	/* Special VMAs are unmergeable with one exception. */
+	if (vmg->flags & VM_SPECIAL) {
+		/*
+		 * VM_RSVD_PLACEHOLDER is treated differently - we know we can
+		 * merge these.
+		 */
+		if (!(vmg->flags & VM_RSVD_PLACEHOLDER))
+			return NULL;
+	}
 
 	can_merge_left = can_vma_merge_left(vmg);
 	can_merge_right = !just_expand && can_vma_merge_right(vmg, can_merge_left);
@@ -1135,6 +1145,7 @@ void vms_complete_munmap_vmas(struct vma_munmap_struct *vms,
 	mm = current->mm;
 	mm->map_count -= vms->vma_count;
 	mm->locked_vm -= vms->locked_vm;
+
 	if (vms->unlock)
 		mmap_write_downgrade(mm);
 
@@ -1182,6 +1193,7 @@ int vms_gather_munmap_vmas(struct vma_munmap_struct *vms,
 {
 	struct vm_area_struct *next = NULL;
 	int error;
+	unsigned long nr_reserved = 0;
 
 	/*
 	 * If we need to split any vma, do it now to save pain later.
@@ -1240,6 +1252,11 @@ int vms_gather_munmap_vmas(struct vma_munmap_struct *vms,
 		vma_mark_detached(next, true);
 		nrpages = vma_pages(next);
 
+		if (vma_is_rsvd_va(next)) {
+			vms->has_rsvd_vmas = true;
+			nr_reserved += nrpages;
+		}
+
 		vms->nr_pages += nrpages;
 		if (next->vm_flags & VM_LOCKED)
 			vms->locked_vm += nrpages;
@@ -1273,6 +1290,14 @@ int vms_gather_munmap_vmas(struct vma_munmap_struct *vms,
 		BUG_ON(next->vm_start < vms->start);
 		BUG_ON(next->vm_start > vms->end);
 #endif
+	}
+
+	if (nr_reserved) {
+		unsigned long total_pages;
+
+		total_pages  = (vms->end - vms->start) >> PAGE_SHIFT;
+		if (total_pages != nr_reserved)
+			vms->partly_spans_reserved = true;
 	}
 
 	vms->next = vma_next(vms->vmi);
@@ -1315,6 +1340,176 @@ map_count_exceeded:
 	return error;
 }
 
+#if VM_RSVD_VA
+static int rsvd_va_mapping_mprotect(struct vm_area_struct *vma, unsigned long start,
+					unsigned long end, unsigned long newflags)
+{
+	return -EINVAL;
+}
+
+static const char *rsvd_va_mapping_name(struct vm_area_struct *vma)
+{
+	return ((char *)"[rsvd]");
+}
+
+static const struct vm_operations_struct rsvd_va_mapping_vmops = {
+	.mprotect	= rsvd_va_mapping_mprotect,
+	.name		= rsvd_va_mapping_name,
+};
+
+int install_rsvd_mapping(struct mm_struct *mm, unsigned long addr,
+				unsigned long len)
+{
+	struct vm_area_struct *vma;
+	unsigned long end = addr + len;
+	unsigned long flags = VM_RSVD_PLACEHOLDER | VM_RSVD_VA | VM_DONTEXPAND | VM_PFNMAP;
+	pgoff_t pgoff = addr >> PAGE_SHIFT;
+	VMA_ITERATOR(vmi, mm, addr);
+	VMG_STATE(vmg, mm, &vmi, addr, end, flags, pgoff);
+	int ret;
+
+	mmap_assert_write_locked(mm);
+
+	vmg.next = vma_next(&vmi);
+	vmg.prev = vma_prev(&vmi);
+	vma_iter_set(&vmi, addr);
+
+	if (vma_merge_new_range(&vmg))
+		goto complete;
+
+	vma = vm_area_alloc(mm);
+	if (unlikely(!vma))
+		return -ENOMEM;
+
+	vma_set_range(vma, addr, end, pgoff);
+	vm_flags_init(vma, flags);
+	vma->vm_page_prot = vm_get_page_prot(vma->vm_flags);
+
+	vma->vm_ops = &rsvd_va_mapping_vmops;
+
+	ret = insert_vm_struct(mm, vma);
+	if (ret) {
+		vm_area_free(vma);
+		return ret;
+	}
+
+complete:
+	vm_stat_account(mm, flags, len >> PAGE_SHIFT);
+
+	return 0;
+}
+
+static int vmi_install_rsvd_placeholder(struct mm_struct *mm, struct vma_iterator *vmi,
+		unsigned long start, unsigned long end)
+{
+	unsigned long flags = VM_RSVD_PLACEHOLDER | VM_RSVD_VA | VM_DONTEXPAND | VM_PFNMAP;
+	unsigned long nrpages;
+	struct vm_area_struct *vma;
+
+	vma = vm_area_alloc(mm);
+	if (unlikely(!vma))
+		return -ENOMEM;
+
+	vma_set_range(vma, start, end, start >> PAGE_SHIFT);
+	vm_flags_init(vma, flags);
+	vma->vm_page_prot = vm_get_page_prot(vma->vm_flags);
+	vma->vm_ops = &rsvd_va_mapping_vmops;
+
+	vma_start_write(vma);
+	if (vma_iter_store_gfp(vmi, vma, GFP_KERNEL))
+		goto mas_store_fail;
+
+	vma_iter_next_range(vmi);
+	mm->map_count++;
+	nrpages = vma_pages(vma);
+	vm_stat_account(mm, flags, nrpages);
+
+	return 0;
+
+mas_store_fail:
+	vm_area_free(vma);
+	return -ENOMEM;
+}
+
+/*
+ * Process the list of vmas that are going away as part of munmap,
+ * and replace any contiguous range of reserved va vmas with a
+ * reserved va placeholder.
+ */
+static int replace_reserved_vmas(struct vma_munmap_struct *vms,
+			struct ma_state *mas_detach)
+{
+	struct vma_iterator *vmi = vms->vmi;
+	struct vm_area_struct *vma;
+	struct mm_struct *mm;
+	unsigned long start, end;
+	bool seen_reserved = false;
+	int error = 0;
+
+	if (!vms->has_rsvd_vmas)
+		return 0;
+
+	vms_clean_up_area(vms, mas_detach);
+	mas_set(mas_detach, 0);
+	mas_for_each(mas_detach, vma, ULONG_MAX) {
+		bool is_reserved = vma_is_rsvd_va(vma);
+
+		if (!is_reserved && !seen_reserved)
+			continue;
+
+		mm = vma->vm_mm;
+
+		if (!seen_reserved) {
+			/* Start a new reserved range. */
+			start = vma->vm_start;
+			end = vma->vm_end;
+			seen_reserved = true;
+			continue;
+		}
+
+		if (!is_reserved) {
+			/* Non-reserved range found after reserved range */
+			error = vmi_install_rsvd_placeholder(mm, vmi, start, end);
+			if (error)
+				return error;
+			seen_reserved = false;
+			continue;
+		}
+
+		if (vma->vm_start != end) {
+			/* Gap between reserved ranges. */
+			error = vmi_install_rsvd_placeholder(mm, vmi, start, end);
+			if (error)
+				return error;
+			start = vma->vm_start;
+			end = vma->vm_end;
+			continue;
+		}
+
+		/* Contiguous reserved range, extend end. */
+		end = vma->vm_end;
+	}
+
+	/* Last vma was reserved */
+	if (seen_reserved)
+		error = vmi_install_rsvd_placeholder(mm, vmi, start, end);
+
+	return error;
+}
+#else
+int install_rsvd_mapping(struct mm_struct *mm, unsigned long addr,
+				unsigned long len)
+{
+	return 0;
+}
+
+static int replace_reserved_vmas(struct vma_munmap_struct *vms,
+		struct ma_state *mas_detach)
+{
+	return 0;
+}
+#endif
+
 /*
  * do_vmi_align_munmap() - munmap the aligned region from @start to @end.
  * @vmi: The vma iterator
@@ -1348,6 +1543,18 @@ int do_vmi_align_munmap(struct vma_iterator *vmi, struct vm_area_struct *vma,
 	error = vma_iter_clear_gfp(vmi, start, end, GFP_KERNEL);
 	if (error)
 		goto clear_tree_failed;
+
+	error = replace_reserved_vmas(&vms, &mas_detach);
+	if (error) {
+		/*
+		 * Warn on failure, for now.
+		 * To be fixed by implementing an unwind to free already
+		 * replaced vmas after which vms_abort_munmap_vmas() can
+		 * be called.
+		 */
+		pr_warn("%s (%d): failure to replace reserved vma after munmap, error %d\n",
+			current->comm, current->pid, error);
+	}
 
 	/* Point of no return */
 	vms_complete_munmap_vmas(&vms, &mas_detach);

mm/vma.h
@@ -42,7 +42,8 @@ struct vma_munmap_struct {
 	int vma_count;                  /* Number of vmas that will be removed */
 	bool unlock;                    /* Unlock after the munmap */
 	bool clear_ptes;                /* If there are outstanding PTE to be cleared */
-	/* 2 byte hole */
+	bool has_rsvd_vmas;		/* rsvd va vmas are being unmapped */
+	bool partly_spans_reserved;	/* Has reserved and unreserved ranges */
 	unsigned long nr_pages;         /* Number of pages being removed */
 	unsigned long locked_vm;        /* Number of locked pages */
 	unsigned long nr_accounted;     /* Number of VM_ACCOUNT pages */
@@ -197,6 +198,8 @@ static inline void init_vma_munmap(struct vma_munmap_struct *vms,
 	vms->unmap_start = FIRST_USER_ADDRESS;
 	vms->unmap_end = USER_PGTABLES_CEILING;
 	vms->clear_ptes = false;
+	vms->has_rsvd_vmas = false;
+	vms->partly_spans_reserved = false;
 }
 #endif
 

xcp-ng/linux

commit 9ccdda54300b475c55451ca35c2b96d218bba8a6

fs/binfmt_elf: validate reserved VA ELF notes
reserve_va_range() trusts the name and descriptor sizes in a reserved-VA
ELF note before proving that the corresponding data fits within the note
segment. A malformed note can therefore make the name comparison or
descriptor reads access beyond the allocated buffer.

Adapt the validation from LUCI 7.1 revision 983a87fd85c4
("mm: Allow userspace to reserve VA range for use by userspace only").
Require the expected name size, verify the complete name and note layout,
require descriptor data to contain complete address pairs, and enforce the
range limit across all matching notes in the segment.

Orabug: 39681042

Fixes: 70b41b4bbbfe ("mm: Allow userspace to reserve VA range for use by userspace only")
Signed-off-by: Jianfeng Wang <jianfeng.w.wang@oracle.com>
Suggested-by: Anthony Yznaga <anthony.yznaga@oracle.com>
Reviewed-by: Mark Tinguely <mark.tinguely@oracle.com>
Reviewed-by: Anthony Yznaga <anthony.yznaga@oracle.com>
Signed-off-by: Sherry Yang <sherry.yang@oracle.com>

fs/binfmt_elf.c
@@ -894,84 +894,106 @@ static int check_preserved_mem_ok(struct linux_binprm *bprm, const char *data, c
 #define MAX_RSVD_VA_RANGES	64
 #define RSVD_VA_STRING		"Reserved VA"
 #define SZ_RSVD_VA_STRING	sizeof(RSVD_VA_STRING)
+#define NT_RSVD_VA		0x07c10001
 
-static int reserve_va_range(const char *note_seg, const size_t note_size)
+static int reserve_va_range(const char *note_seg, const size_t note_seg_size)
 {
+	const char *note_seg_end;
 	struct elf_note *note;
 	int retval = 0;
+	int nr_total_ranges = 0;
 
+	if (!note_seg || !note_seg_size)
+		return 0;
+
+	note_seg_end = note_seg + note_seg_size;
 	note = (struct elf_note *)note_seg;
-	while ((char *)note + sizeof(struct elf_note) <
-	       (char *)(note_seg + note_size)) {
-		char *name;
+	while (((char *)note + sizeof(struct elf_note)) < note_seg_end) {
+		char *note_end, *name;
 		unsigned long *val;
 		unsigned long nentry, i;
+		int nr_ranges;
 
-		if (note->n_type != 0x07c10001)
+		note_end = ((char *)note + sizeof(struct elf_note) +
+				roundup((unsigned long)note->n_namesz, 4) +
+				roundup((unsigned long)note->n_descsz, 4));
+
+		/*
+		 * The ELF spec says that both name and type must be
+		 * recognized before assuming the format of note descriptor
+		 * data. Ignore notes where either value does not or
+		 * cannot be matched.
+		 */
+		if (note->n_type != NT_RSVD_VA)
 			goto cont_loop;
 
-		/* Sanity check for malformed note entry */
-		if (note->n_namesz > SZ_RSVD_VA_STRING) {
+		if (note->n_namesz != SZ_RSVD_VA_STRING)
+			goto cont_loop;
+
+		name = (char *)note + sizeof(struct elf_note);
+		if (name + SZ_RSVD_VA_STRING > note_seg_end)
+			goto cont_loop;
+
+		if (strncmp(name, RSVD_VA_STRING, SZ_RSVD_VA_STRING) != 0)
+			goto cont_loop;
+
+		/*
+		 * Does the ELF note header plus the advertised sizes for
+		 * the name and data associated with it fit within the note
+		 * segment?
+		 */
+		if (note_end > note_seg_end) {
 			retval = -ENOEXEC;
 			goto out;
 		}
 
-		name = (char *)note + sizeof(struct elf_note);
-		if (strncmp(name, RSVD_VA_STRING, SZ_RSVD_VA_STRING) == 0) {
-			nentry = note->n_descsz/sizeof(void *);
-			val = (unsigned long *)(name +
-						roundup(note->n_namesz, 4));
-			/*
-			 * Check if right number of address
-			 * entries exist in note section
-			 */
-			if (((nentry % 2) != 0) ||
-				((nentry / 2) > MAX_RSVD_VA_RANGES)) {
+		/*
+		 * The descriptor data is expected to contain one or more
+		 * pairs of 8-byte addresses with each pair representing the
+		 * start and end addresses of a VA range to reserve.
+		 */
+		if (!IS_ALIGNED(note->n_descsz, sizeof(void *) * 2)) {
+			retval = -ENOEXEC;
+			goto out;
+		}
+
+		nentry = note->n_descsz / sizeof(void *);
+		nr_ranges = nentry / 2;
+		if (nr_total_ranges + nr_ranges > MAX_RSVD_VA_RANGES) {
+			retval = -ENOEXEC;
+			goto out;
+		}
+
+		val = (unsigned long *)(name + roundup(note->n_namesz, 4));
+		for (i = 0 ; i < nentry; i += 2) {
+			unsigned long range1, range2, size;
+			struct mm_struct *mm = current->mm;
+
+			range1 = PAGE_ALIGN_DOWN(*val++);
+			range2 = PAGE_ALIGN(*val++);
+			size = range2 - range1;
+
+			/* Validate the address range being reserved */
+			if (range2 <= range1 ||
+			    !access_ok((void *)range1, size)) {
 				retval = -ENOEXEC;
 				goto out;
 			}
-			for (i = 0 ; i < nentry; i += 2) {
-				unsigned long range1, range2, size;
-				struct mm_struct *mm = current->mm;
 
-				/*
-				 * Ensure we can access two address entries
-				 * in this note segment safely
-				 */
-				if ((char *)(val + 1) >=
-					((char *)note_seg + note_size)) {
-					retval = -ENOEXEC;
-					goto out;
-				}
-				range1 = PAGE_ALIGN((*val++) - PAGE_SIZE + 1);
-				range2 = PAGE_ALIGN(*val++);
-				size = range2 - range1;
+			/*
+			 * install_rsvd_mapping() requires the mmap
+			 * write lock be held by the caller.
+			 */
+			mmap_write_lock(mm);
+			retval = install_rsvd_mapping(mm, range1, size);
+			mmap_write_unlock(mm);
 
-				/* Validate the address range being reserved */
-				if ((range2 <= range1) ||
-				    (!access_ok((void *)range1, size))) {
-					retval = -ENOEXEC;
-					goto out;
-				}
-
-				/*
-				 * install_rsvd_mapping() requires the mmap
-				 * write lock be held by the caller. Needed vma
-				 * write locks will be taken later.
-				 */
-				mmap_write_lock(mm);
-				retval = install_rsvd_mapping(mm, range1, size);
-				mmap_write_unlock(mm);
-
-				if (retval < 0)
-					goto out;
-			}
+			if (retval < 0)
+				goto out;
 		}
+		nr_total_ranges += nr_ranges;
 cont_loop:
-		note = (struct elf_note *)((char *)note +
-						sizeof(struct elf_note) +
-						roundup(note->n_namesz, 4) +
-						roundup(note->n_descsz, 4));
+		note = (struct elf_note *)note_end;
 	}
 
 out: